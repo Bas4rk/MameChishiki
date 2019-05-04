c4956df2f733077658b56c47dbdb29137bfc7b24
NatuKumo <yakikazu6@gmail.com>
2019-05-05 02:48:15 +0900

    login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,23 @@
 class ApplicationController < ActionController::Base
    # 以下を書くだけでよい
   prepend_view_path Rails.root.join("frontend")
+
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  def after_sign_in_path_for(resource)
+    pages_show_path
+  end
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    end
+
+  private
+    def sign_in_required
+      redirect_to new_user_session_url unless user_signed_in?
+    end
 end
--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -1,5 +1,6 @@
 import '../stylesheets/application';
 import '../javascripts/application';
 require.context('../images', true, /\.(png|jpg|jpeg|svg)$/);
+import Rails from 'rails-ujs'
+Rails.start()
 
-alert("hello")
