4ef993bea982156a7aeca019f31eb3ca1d51172c
Akiba <Akiba@localhost.localdomain>
2019-07-01 18:21:48 +0900

    comment_fix

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,10 +7,17 @@ class CommentsController < ApplicationController
    if @comment.save
       redirect_back(fallback_location: root_path)
     else
-      redirect_to user_url(current_user)
+      redirect_back(fallback_location: root_path)
     end
   end
 
+  def destroy
+    @post=Post.find(params[:post_id])
+    @comment = @post.comments.find_by(id: params[:id])
+    @comment.destroy
+    flash[:success] = "comment deleted"
+    redirect_to request.referrer || root_url
+  end
   private
 
   def comment_params
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 }
   resources  :users, :only => [:show,:index,:edit,:update]
   resources :posts, :only => [:show,:new,:create,:destroy] do
-    resources :comments, :only =>  [:create]
+    resources :comments, :only =>  [:create,:destroy]
   end
  # get '/users/:id' => 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
